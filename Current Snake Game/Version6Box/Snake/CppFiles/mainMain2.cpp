e54b26725d6c4f2c1828fc267af8e2295d7459d1
BryanAtkinsonJ <bryana@business-analyst.me>
2019-03-21 01:10:36 -0400

    Testing Commit to Github

--- Current Snake Game/Version6Box/Snake/CppFiles/mainMain2.cpp
+++ Current Snake Game/Version6Box/Snake/CppFiles/mainMain2.cpp
@@ -4,8 +4,8 @@
 	#include "../Headers/GridPiece.h"
 
 	// See Version 1 - 4 for info on using templates
+  // TestCommit
 
-	
 
 	int main(int argc, char **argv)
 	{
@@ -14,26 +14,26 @@
 				//To_Implement: Children Class
 		std::unique_ptr<GameBoxes::Box> squareBackground(new GameBoxes::Box(std::stoi(argv[1]), std::stoi(argv[2]),
 								   std::stoi(argv[3]), std::stoi(argv[4])));
-		
+
 		int cArgs[4];
 		(*squareBackground).getSizeArray(cArgs);
-		
-		sf::RenderWindow window( sf::VideoMode(cArgs[0], cArgs[1]), "SFML window");	
+
+		sf::RenderWindow window( sf::VideoMode(cArgs[0], cArgs[1]), "SFML window");
 		sf::Image background;
 		sf::Texture texture;
 		(*squareBackground).createBackGroundTexture(background, texture);
 			//create a background and a texture, then initialize them in the class that handles it.
 		sf::Sprite sprite(texture);
 			//create the sprite for drawing
-		
+
 		// Create GridPieces : needs squareArray to inherit from it
 		typedef std::shared_ptr<GameBoxes::GridPiece> GridSharedPointer;
 		typedef std::vector< GridSharedPointer > GridPieceVector;
 		std::vector < GridPieceVector > twoDGridVector;
 			// Uses a typedef to create a shared_pointer
 			// Uses a typedef to create a shared_pointer container of class objects
-			// Declares a 2D vector	
-		
+			// Declares a 2D vector
+
 		for (int x_index = 0; x_index < cArgs[0] / cArgs[2]; x_index++) // Number of blocks in row
 		{
 			GridPieceVector rowOfGridPiece;
@@ -48,7 +48,7 @@
 			}
 		}
 				// See version 4 for debugging
-		
+
 			// Declaring time variables
 			// https://stackoverflow.com/questions/20957630/my-sfml-c-program-is-working-as-expected-but-how
 			// -to-make-movement-slow/20958283#20958283
@@ -57,14 +57,14 @@
 		sf::Time timeSinceLastUpdate = sf::Time::Zero;
 		int x_index = 0, y_index = 0; // indexes for twoDGridVector
 		bool dead = true;
-		
+
 		while (window.isOpen())
         {
 			sf::Event event;
 			window.setVerticalSyncEnabled(true);
-			
+
 			window.display();
-			
+
 			if (dead == true)
 			{
 				window.clear();
@@ -72,20 +72,20 @@
 				dead = false;
 					// create snake here
 			}
-			
+
 			sf::Time deltaTime = deltaClock.restart(); // Restart resets time
 			timeSinceLastUpdate += deltaTime;
-			
+
 			while (timeSinceLastUpdate >= timePerFrame || (x_index == 0 && y_index == 0))
 				// Eventually use this to move the snake
 			{
-				
+
 				window.draw(*(twoDGridVector[x_index][y_index])); // Draw a GridPiece
 					// Move snake here instead and (x_index == 0 && y_index == 0) dissapears.
 					// Move in random direction at the start, then in last moved direction otherwise
 				timeSinceLastUpdate = sf::Time::Zero;
 
-				
+
 				y_index = (y_index + 1) % (cArgs[1] / cArgs[3]);
 				if (y_index == 0)
 				{
@@ -93,22 +93,22 @@
 				}
 					// The above code increments the double for loop every time deltaTime reaches a
 					// threshhold.
-				
+
 				break; // Stops while loop from immediately drawing Grid (0,0) when it resets)
 			}
-			
+
             // handle all events
             while (window.pollEvent(event))
             {
-				
-				
-				
+
+
+
 				// Make double threaded program where the main thread (drawing all squares) is constantly redrawing the
 				// entire board (eventually just the snake) at a rate of one square every 2.5 seconds. Then there is a
 				// second thread which 1) asks for user input (choose a square and change its color) and 2) redraws the
 				// specified square (eventually the snake). When user input is given via 1) the threads join and 2) happens
 				// BEFORE the main thread begins redrawing all the squares as a single square every 2.5 seconds.
-				
+
 				/*
 				sf::Clock clock;
 				for (int x_index = 0; x_index < cArgs[0] / cArgs[2]; x_index++)
@@ -117,7 +117,7 @@
 					{
 						if (clock.getElapsedTime().asSeconds() >= 1)
 						{
-							sf::Time elapsed = clock.restart();	
+							sf::Time elapsed = clock.restart();
 							window.draw(*(twoDGridVector[x_index][y_index]));
 						}
 
@@ -127,13 +127,13 @@
 
 				// Remember to declare each GridPiece as "GridPiece * squareName = new GridPiece(Init)"
 					//Then when you are done, set all the pointers to nullptr and delete everything.
-				
+
 				// TODO: Start from bottom right and build out the squares right -> left and bottom -> top
 				// such that the maximum possible space is filled. Create a 2D array of GridPiece's for
 				// drawing. Then, once they are all drawn go through and hook the pointers up to every
 				// GridPiece to the top, left, bottom and right. If a side is an end, make it a NULLPOINTER
 					//calculate the size of the array by using getSizeArray from Box.h/Box.cpp
-				
+
 				// Have a fifth pointer, a int variable, and a Move function in GridPiece.
 				// A Gridpiece variable in main will be the head of the snake, and the fifth pointer will
 				// point to the next block of snake behind it. The int variable will say what position in
@@ -146,23 +146,23 @@
 				// If the Snake eats an apple, the new head is given +1 value of the current head or greater,
 				// rather than == to it and none of the blocks behind are changed. If the value given is >+1,
 				// then randomly grow the snake from the tail until a GridPiece has int value 1.
-				
+
 				// When creating the snake, choose a random spot on the grid as the head and grow it randomly
 				// in the available directions. If the snake dies, reset the board and randomize snake again.
-				
+
                 //Close window: exit
                 if (event.type == sf::Event::Closed)
                             window.close();
             }
             // update
-			
+
 			if (x_index == 0 && y_index == 0)
 			{
 				dead = true;
 				// The x_index == 0 && y_index == 0 condition is replaced with the snake
 				// hitting its own body or a wall
 			}
-			
+
 			// Have an if statement where if the snake eats an apple, it random grows. Then spawn another apple.
 
         }
